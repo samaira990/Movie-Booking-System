3ec804ea9d71f79625138d617e2da6e6d1168eac
Samaira <samairabhatti61@gmail.com>
2026-04-18 21:33:02 +0530

    Initial commit

---
+++ users/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+
+    class Meta():
+      model = User
+      fields = ["username", "email", "password1", "password2"]
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta():
+      model = User
+      fields = ["username", "email"]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta():
+      model = User #if adding more profile fields, change to the Profile Model
+      fields = ["password"] # User can reset the password
+
