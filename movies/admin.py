97e2074a06af1b794c7b1948cacf377641fdfc61
Samaira <samairabhatti61@gmail.com>
2026-04-23 11:04:09 +0530

    improved admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Movie, Theater, Seat,Booking
+from .models import Movie, Theater, Seat, Booking, Genre, Language
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['name', 'rating', 'cast','description']
+    filter_horizontal = ['genres', 'languages']  
+
 
 @admin.register(Theater)
 class TheaterAdmin(admin.ModelAdmin):
@@ -16,3 +18,13 @@ class SeatAdmin(admin.ModelAdmin):
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     list_display = ['user', 'seat', 'movie','theater','booked_at']
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
