56983923bc3be5e4ab413099f4ebdee42a0b8e30
Samaira <samairabhatti61@gmail.com>
2026-06-01 12:58:13 +0530

    fix vercel config for django static handling

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -186,6 +186,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
 
 
 LOGGING = {
